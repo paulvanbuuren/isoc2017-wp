a9b743fb2c1356e93bfa505afa665d41bd2ffd3f
Paul van Buuren <paul@wbvb.nl>
2021-05-20 12:32:55 +0200

    Removed inclusion of browsersync script for local development
    This fixes issues:
    https://github.com/InternetSocietyChapters/isoc2017-wp/issues/18
    https://github.com/InternetSocietyChapters/isoc2017-wp/issues/19
    https://github.com/InternetSocietyChapters/isoc2017-wp/issues/20

--- footer.php
+++ footer.php
@@ -8,11 +8,5 @@
 	<?php Template::Render('site-footer'); ?>
 	<?php wp_footer(); ?>
 
-	<?php if(OD_ENV == 'local') : ?>
-		<!-- browser sync -->
-		<script id="__bs_script__">//<![CDATA[
-	document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.12.10.js'><\/script>".replace("HOST", location.hostname));
-//]]>	</script>
-	<?php endif; ?>
 	</body>
 </html>
